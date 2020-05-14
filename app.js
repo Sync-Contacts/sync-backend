5edf15c093921bd5287a8f6e3e44ccf99e1c426d
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-13 20:06:44 -0600

    Modify app to utilize bodyParser and routes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const users = require('./routes/users')
 const contacts = require('./routes/contacts')
 const topics = require('./routes/topics')
 
 const app = express();
 
-app.use('/users', users)
-app.use('/contacts', contacts)
-app.use('/topics', topics)
+app.use(bodyParser.json());
+app.use('/users', users);
+app.use('/contacts', contacts);
+app.use('/topics', topics);
 
 app.use((err, request, response, next) => {
     response.json(err)
