24a270add730a32d73d85119eacc72fb32990a9e
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-13 20:06:54 -0600

    Add contact post endpoint

--- routes/contacts.js
+++ routes/contacts.js
@@ -14,4 +14,33 @@ router.get('/:id', (request, response, next) => {
     });
 });
 
+router.post('/:id', (request, response, next) => {
+    const { id } = request.params;
+    const {
+        linked_user_id,
+        contact_name,
+        phone,
+        email,
+        contact_address,
+        last_contact,
+        contact_frq
+    } = request.body;
+
+    pool.query('INSERT INTO Contacts(id, linked_user_id, contact_name, phone, email, contact_address, last_contact, contact_frq) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
+    [id,
+    linked_user_id,
+    contact_name,
+    phone,
+    email,
+    contact_address,
+    last_contact,
+    contact_frq],
+    (err, resp) => {
+        if (err) return next(err);
+
+        resp.redirect(`/contacts/${id}`)
+    }
+    );
+})
+
 module.exports = router;
